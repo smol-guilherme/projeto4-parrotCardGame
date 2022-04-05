292352eaafa15fae0cf33f691eb09ddffd2db3be
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-04-05 19:42:56 -0300

    Nova funcionalidade. O jogo agora distribui as cartas de maneira parcialmente correta. As imagens carregam corretamente. Alterada estrutra das pastas e nomes de arquivos de conteúdo.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,34 @@
 function main() {
-    initializeGame()
+    initializeGame();
 }
 
+main();
+
 function initializeGame() {
-    let input = null;
-    while(input !== 'x') {
+    let input = -1;
+    while(input % 2 !== 0) {
         input = prompt("Escolha um número par de cartas: \n(Min: 4, Max: 14)\n(Insira X para sair)")
         if (input % 2 === 0)
-            pickDeck(input)
-            return 1;
+            pickDeck(input);
     }
     restartGame();
 }
 
 function restartGame() {
+    let input = undefined;
+
     while (input !== "não"){
-        const input  = prompt("Gostaria de jogar novamente? (sim / não)");
-        if (input === "sim")
+        input  = prompt("Gostaria de jogar novamente? (sim / não)");
+        if (input === "sim") {
             initializeGame();
-            break;
+        }
     }
+    endGame();
+}
+
+function endGame() {
     alert("Obrigado por jogar o magnífico e modesto Parrot Card Game");
+    return 0;
 }
 
 function pickDeck(deck) {
@@ -42,10 +50,22 @@ function shuffle() {
     return Math.random() - 0.5;
 }
 
-
 function dealCards(deck) {
     deck.sort(shuffle)
+    const board = document.querySelector(".card-board");
     for(i = 0; i < deck.length; i++) {
-        
+        let card = document.createElement("div");
+        let imgFront = document.createElement("img");
+        let imgBack = document.createElement("img");
+        card = board.appendChild(card);
+        card.classList.add("card-element");
+        card.appendChild(imgFront);
+        imgFront.classList.add("card-front");
+        imgFront.setAttribute("src", "./images/gui/front.png")
+        card.appendChild(imgBack);
+        imgBack.classList.add("card-back");
+        imgBack.setAttribute("src", `./images/content/0${deck[i]}_parrot.gif`)
+        imgBack.classList.add("hidden");
     }
+    playGame();
 }
