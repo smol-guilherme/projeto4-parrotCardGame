3b52deab7f1d85fae72337eca87df3708ffbd26d
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-04-07 09:07:35 -0300

    Alterada implementação da adição de elementos.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,24 +5,24 @@ function main() {
 main();
 
 function initializeGame() {
-    let input = -1;
-    while(input % 2 !== 0) {
-        input = prompt("Escolha um número par de cartas: \n(Min: 4, Max: 14)\n(Insira X para sair)")
-        if (input % 2 === 0)
-            pickDeck(input);
-    }
-    restartGame();
+    let input;
+    do {
+        input = prompt("Escolha um número par de cartas: \n(Min: 4, Max: 14)");
+        input = Number(input)
+    } while (input < 4 || input % 2 !== 0 || input > 14)
+    pickDeck(input); 
+    // restartGame();
 }
 
 function restartGame() {
-    let input = undefined;
+    let input;
 
-    while (input !== "não"){
+    do {
         input  = prompt("Gostaria de jogar novamente? (sim / não)");
         if (input === "sim") {
             initializeGame();
         }
-    }
+    } while (input !== "não")
     endGame();
 }
 
@@ -50,22 +50,47 @@ function shuffle() {
     return Math.random() - 0.5;
 }
 
+function flipCard(card) {
+    card.querySelector(".card-front").classList.toggle("hidden")
+    card.querySelector(".card-back").classList.toggle("hidden")
+    setTimeout(() => {
+        console.log("timeout started")
+        card.querySelector(".card-front.hidden").classList.remove("hidden")
+        card.querySelector(".card-back").classList.add("hidden")    
+    }, 1000);
+}
+
+function unflipCard(card) {
+    card.querySelector(".card-front").classList.toggle("hidden")
+    card.querySelector(".card-back").classList.toggle("hidden")
+}
+
 function dealCards(deck) {
     deck.sort(shuffle)
-    const board = document.querySelector(".card-board");
+    const board = document.querySelector("main");
     for(i = 0; i < deck.length; i++) {
-        let card = document.createElement("div");
-        let imgFront = document.createElement("img");
-        let imgBack = document.createElement("img");
-        card = board.appendChild(card);
-        card.classList.add("card-element");
-        card.appendChild(imgFront);
-        imgFront.classList.add("card-front");
-        imgFront.setAttribute("src", "./images/gui/front.png")
-        card.appendChild(imgBack);
-        imgBack.classList.add("card-back");
-        imgBack.setAttribute("src", `./images/content/0${deck[i]}_parrot.gif`)
-        imgBack.classList.add("hidden");
+        board.innerHTML +=
+        `
+        <div onclick="flipCard(this)" class="card-element">
+            <img class="card-front" src="./images/gui/front.png" alt="">
+            <img class="card-back hidden" src="./images/content/0${deck[i]}_parrot.gif" alt="">
+        </div>
+        `
+        // let card = document.createElement("div");
+        // let imgFront = document.createElement("img");
+        // let imgBack = document.createElement("img");
+        // card = board.appendChild(card);
+        // card.classList.add("card-element");
+        // card.setAttribute("onclick", "flipCard(this)");
+        // card.appendChild(imgFront);
+        // imgFront.classList.add("card-front");
+        // imgFront.classList.add("card-front", "hidden");
+        // imgFront.setAttribute("src", "./images/gui/front.png")
+
+        // card.appendChild(imgBack);
+        // imgBack.classList.add("card-back", "hidden");
+        // imgBack.classList.add("card-back");
+        // imgBack.setAttribute("src", `./images/content/0${deck[i]}_parrot.gif`)
     }
     playGame();
 }
